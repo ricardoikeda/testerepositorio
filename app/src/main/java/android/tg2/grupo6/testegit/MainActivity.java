2c18927280444f1c5c333eff6375f993527625ab
Ricardo Ikeda <ricardo.ikeda@outlook.com>
2022-04-16 12:20:33 -0300

    Iniciar commit

---
+++ app/src/main/java/android/tg2/grupo6/testegit/MainActivity.java
@@ -0,0 +1,14 @@
+package android.tg2.grupo6.testegit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
